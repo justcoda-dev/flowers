71ebb26cff91d4ca8449a88a0f370d32b349513c
oleksandr.koval <koval.develop@gmail.com>
2022-12-09 15:23:04 +0200

    add config

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [{
+    name: "NuxtAppName",
+    exec_mode: 'cluster',
+    instances: 'max',
+    script: './node_modules/nuxt/bin/nuxt.js',
+    args: 'start'
+  }]
+}
